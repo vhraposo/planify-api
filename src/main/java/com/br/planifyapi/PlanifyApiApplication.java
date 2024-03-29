10f48f2d8bb3c045cad00955a7cf722bbb1f9b12
vhraposo <thevhup@gmail.com>
2024-03-29 17:22:51 -0300

    Create maven project

---
+++ src/main/java/com/br/planifyapi/PlanifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.planifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanifyApiApplication.class, args);
+	}
+
+}
